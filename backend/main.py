9069402933b0e7a375b8eb718df9447ebad7453d
Filipe Colla David <filipecolladavid@gmail.com>
2022-12-13 16:30:54 +0000

    Added endpoints for inserting images in database and bucket

--- backend/main.py
+++ backend/main.py
@@ -1,59 +1,49 @@
-import json, os
-from fastapi import FastAPI, UploadFile
-from minio import Minio, InvalidResponseError
+import os
+from fastapi import FastAPI, UploadFile, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from minio import InvalidResponseError
 from urllib import parse
+from storage import minio_client, bucket
+from database import db
+from models import Post, UpdatePost
+from config import settings
 
 app = FastAPI()
 
-minio_client = Minio(
-    "minio:9000",
-    access_key="minio",
-    secret_key="minio123",
-    secure=False
-)
-
-bucket = "media"
-
-if not minio_client.bucket_exists(bucket):
-    minio_client.make_bucket(bucket)
-
-bucket_policy = json.dumps(
-    {
-        "Statement": [
-            {
-                "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
-                "Effect": "Allow",
-                "Principal": {
-                    "AWS": ["*"]
-                },
-                "Resource": [f"arn:aws:s3:::{bucket}"]
-            }, {
-                "Action": ["s3:GetObject"],
-                "Effect": "Allow",
-                "Principal": {
-                    "AWS": ["*"]
-                },
-                "Resource": [f"arn:aws:s3:::{bucket}/*"]
-            }],
-        "Version": "2012-10-17"
-    }
-)
-
-result = minio_client.set_bucket_policy(bucket, bucket_policy)
 
 @app.get("/")
 def get_root():
-    return {"status":"ok"}
+    return {"status": "ok"}
+
+# minio:9000 using for testing 127.0.0.1
+baseUrl = "http://127.0.0.1:9000/"
+
 
 @app.post("/upload")
-def upload(image: UploadFile, name: str = "Image", desc: str = "No Description Available"):
+def upload(image: UploadFile, title: str = "Image", desc: str = "No Description Available", date: str = "1/1/2000"):
+
     file_size = os.fstat(image.file.fileno()).st_size
+    file_name = title+(date.replace("/","_"))
     try:
-        result = minio_client.put_object(bucket, "test.png", image.file, file_size, image.content_type)
-        publicUrl = "http://127.0.0.1:9000/media/"+parse.quote("test.png")
-        return {"message": publicUrl}
+        minio_client.put_object(bucket, file_name, image.file, file_size, image.content_type)
+        publicUrl = baseUrl+bucket+"/"+parse.quote(file_name)
     except InvalidResponseError as err:
         return {"message": err.message}
+
+    post = Post(
+        title=title,
+        description=desc,
+        src=publicUrl,
+        date=date,
+    )
+
+    json_post = jsonable_encoder(post)
+    new_student = db[settings.MONGO_INITDB_DATABASE].insert_one(json_post)
+    created_student = db[settings.MONGO_INITDB_DATABASE].find_one({"_id": new_student.inserted_id})
+    
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
 
 @app.get("/images")
 def get_image_urls():
@@ -63,9 +53,8 @@ def get_image_urls():
         for obj in objects:
             image_url = minio_client.presigned_get_object(
                 bucket, obj.object_name)
-            image_urls.append(image_url.split("?")[0].replace("minio","127.0.0.1"))
+            image_urls.append(image_url.split(
+                "?")[0].replace("minio", "127.0.0.1"))
         return {"image_urls": image_urls}
     except InvalidResponseError as err:
-        return {"message": err.message}    
-    
-
+        return {"message": err.message}
