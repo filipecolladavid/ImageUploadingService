482db295a5c44d6dea8992ddd10c20804843eda5
Filipe Colla David <filipecolladavid@gmail.com>
2022-12-15 22:39:54 +0000

    Added frontend to docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {loading ? <>Loading</> : <>{text}</>}
+        <button
+          onClick={() => {
+            setLoading(!loading);
+            fetch("http://api:8000/").then((response) => {
+              let r = response.json();
+              setText(r.status);
+              setLoading(false);
+              console.log(r)
+            });
+          }}
         >
-          Learn React
-        </a>
+          Click me
+        </button>
       </header>
     </div>
   );
